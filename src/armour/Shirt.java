36e60463e98bf7e580dc93e2286192d3cf448fb7
John Yenter-Briars <jyenterbriars@gmail.com>
2022-07-13 06:52:49 -0500

    reformat against java conventions

--- src/armour/Shirt.java
+++ src/armour/Shirt.java
@@ -1,13 +1,14 @@
 package armour;
 
 import java.io.Serializable;
-public class Shirt extends Armour implements Serializable{
 
-  /* Class Variables */
-private static final long serialVersionUID = 1L;
+public class Shirt extends Armour implements Serializable {
 
-  /* Constructors */
-  public Shirt(String name, int durability, int defenseRating) {
-    super(name, durability, defenseRating);
-  }
+	/* Class Variables */
+	private static final long serialVersionUID = 1L;
+
+	/* Constructors */
+	public Shirt(String name, int durability, int defenseRating) {
+		super(name, durability, defenseRating);
+	}
 }
